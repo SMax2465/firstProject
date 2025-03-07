d6b405d5e7a76854733ed61529426521e148c98a
1Shain <sony.hadams@gmx.fr>
2025-03-07 14:36:25 +0100

    System.out.println("How are you ?");

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome everyone!");
+        System.out.println("How are you ?");
         System.out.println("Hello and welcome! Maxime");
 
         for (int i = 1; i <= 5; i++) {
