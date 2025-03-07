656789847e729291b75e1e1c3404f350e895eaf9
SMax.2465 <maximesoupgui@gmail.com>
2025-03-07 14:36:08 +0100

    System.out.println("How are you to day?");

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome everyone!");
-        System.out.println("How are you ?");
+        System.out.println("How are you to day?");
         System.out.println("Hello and welcome! Maxime");
         System.out.println("Hello and welcome! Smax");
         for (int i = 1; i <= 5; i++) {
