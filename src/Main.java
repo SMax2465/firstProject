bba5e2a3d609abdfdbecc209e5fb1876ca1e5dd0
mohammed <mohammed.khedim@etu.u-pec.fr>
2025-03-07 14:16:47 +0100

    code modifié

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
-
-        System.out.println(somme(2,3));
-    }
-
-    public static int somme(int a, int b){
-        return a+b;
-    }
-    public static int multiplication(int a, int b){
-        return a*b;
+        System.out.println("nouveau commit");
     }
 
 }
