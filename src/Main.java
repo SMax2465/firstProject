d64901e0351f6c0485c002c62d2a0c5d8ed2c363
SMax.2465 <maximesoupgui@gmail.com>
2025-03-07 14:32:30 +0100

    smax

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome everyone!");
         System.out.println("Hello and welcome! Maxime");
-
+        System.out.println("Hello and welcome! Smax");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
